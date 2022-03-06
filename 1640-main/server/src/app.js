6f04b0c145e81421add2a5eb045a3878aa2e2678
bombom28821 <bombom28821@gmail.com>
2022-03-06 17:34:26 +0700

    update

--- 1640-main/server/src/app.js
+++ 1640-main/server/src/app.js
@@ -1,14 +1,14 @@
-import  express  from 'express'
-import db from './config/db.js'
-import path from 'path'
+import express from "express";
+import db from "./config/db.js";
+import path from "path";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", express.static('../../client'));
+app.use("/", express.static("../../client"));
 const run = async () => {
-    await db.connect();
-}
+  await db.connect();
+};
 run();
-
-app.listen(3000, ()=> console.log('Server is running !'))
+hi;
+app.listen(3000, () => console.log("Server is running !"));
